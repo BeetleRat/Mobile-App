ba7ba3c71ec3c2764309d6d50f4a262b7867e28c
BeetleRat <86663719+BeetleRat@users.noreply.github.com>
2022-02-13 02:41:41 +0300

    Лабораторная работа 2.
    Создано простое приложение.
    Добавлены кнопки, строковые константы.
    В слушателей кнопок добавлены заглушки.

--- app/src/main/java/ru/beetlerat/mobile_app/MainActivity.kt
+++ app/src/main/java/ru/beetlerat/mobile_app/MainActivity.kt
@@ -2,10 +2,70 @@ package ru.beetlerat.mobile_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    // Объявление объектов разметки
+    private lateinit var calculatorActivityButton:Button
+    private lateinit var cameraActivityButton:Button
+    private lateinit var locationActivityButton:Button
+    private lateinit var databaseActivityButton:Button
+    private lateinit var restActivityButton:Button
+
+    private fun initElements(){
+        calculatorActivityButton=findViewById(R.id.calculatorActivityButton)
+        calculatorActivityButton.setText(R.string.calculator_activity_button)
+
+        cameraActivityButton=findViewById(R.id.cameraActivityButton)
+        cameraActivityButton.setText(R.string.camera_activity_button)
+
+        locationActivityButton=findViewById(R.id.locationActivityButton)
+        locationActivityButton.setText(R.string.location_activity_button)
+
+        databaseActivityButton=findViewById(R.id.databaseActivityButton)
+        databaseActivityButton.setText(R.string.database_activity_button)
+
+        restActivityButton=findViewById(R.id.restActivityButton)
+        restActivityButton.setText(R.string.rest_activity_button)
+    }
+
+    private fun addListenersToElements(){
+        calculatorActivityButton.setOnClickListener {
+            // Вывести заглушку на экран
+            Toast.makeText(this,R.string.calculator_activity_button_pressed,Toast.LENGTH_LONG).show()
+        }
+
+        cameraActivityButton.setOnClickListener {
+            // Вывести заглушку на экран
+            Toast.makeText(this,R.string.camera_activity_button_pressed,Toast.LENGTH_LONG).show()
+        }
+
+        locationActivityButton.setOnClickListener {
+            // Вывести заглушку на экран
+            Toast.makeText(this,R.string.location_activity_button_pressed,Toast.LENGTH_LONG).show()
+        }
+
+        databaseActivityButton.setOnClickListener {
+            // Вывести заглушку на экран
+            Toast.makeText(this,R.string.database_activity_button_pressed,Toast.LENGTH_LONG).show()
+        }
+
+        restActivityButton.setOnClickListener {
+            // Вывести заглушку на экран
+            Toast.makeText(this,R.string.rest_activity_button_pressed,Toast.LENGTH_LONG).show()
+        }
+    }
+
+    // Метод запускаемый при создании activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Установка разметки которую отображает данная activity
         setContentView(R.layout.activity_main)
+
+        initElements()
+
+        addListenersToElements()
     }
 }
